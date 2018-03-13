b8160d6cb240bd9e1c27c0c815b65cc1f954f7d1
Michelle Hoffman <hoffman.michelle.e@gmail.com>
2018-03-13 11:43:00 -0600

    Outline what endpoints are needed for server.js

--- server.js
+++ server.js
@@ -6,6 +6,30 @@ app.locals.title = 'Palette Picker';
 
 app.use(express.static('public'));
 
+app.get('/api/v1/projects', (request, response) => {
+  // read all projects
+})
+
+app.get('/api/v1/palettes', (request, response) => {
+  // read all pallets
+})
+
+app.get('/api/v1/palettes/:id', (request, response) => {
+  // get a certain palette to display on the page
+})
+
+app.post('/api/v1/projects', (request, response) => {
+  // create a new project
+})
+
+app.post('/api/v1/palettes', (request, response) => {
+  // create a new palette
+})
+
+app.delete('/api/v1/palettes/:id', (request, response) => {
+  // delete a certain palette
+})
+
 app.listen(app.get('port'), () => {
   console.log(`${ app.locals.title } is listening on port ${ app.get('port') }`)
 })
