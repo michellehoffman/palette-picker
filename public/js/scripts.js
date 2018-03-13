af573159b7f7becee61b97d1fae653ceb322b45a
Michelle Hoffman <hoffman.michelle.e@gmail.com>
2018-03-12 20:33:20 -0600

    Add ability to lock colors

--- public/js/scripts.js
+++ public/js/scripts.js
@@ -1,6 +1,19 @@
 window.onload = displayColors;
-
 $('.generate-button').on('click', displayColors)
+$('.lock-button').on('click', lockColor);
+
+const palette = [
+  { div: '.color-1', color: null, locked: false },
+  { div: '.color-2', color: null, locked: false },
+  { div: '.color-3', color: null, locked: false },
+  { div: '.color-4', color: null, locked: false },
+  { div: '.color-5', color: null, locked: false }
+]
+
+const lockImage = {
+  true: './assets/002-lock-1.svg',
+  false: './assets/003-lock.svg'
+}
 
 function generateColor() {
   const letters = '0123456789ABCDEF';
@@ -14,12 +27,20 @@ function generateColor() {
 }
 
 function displayColors() {
-  const palette = ['.color-1', '.color-2', '.color-3', '.color-4', '.color-5'];
+  palette.forEach( section => {
+    if(!section.locked) {
+      section.color = generateColor();
 
-  palette.forEach( div => {
-    const color = generateColor();
-
-    $(div).css('background-color', color)
-    $(div).find('h2').text(color);
+      $(section.div).css('background-color', section.color)
+    }
+    $(section.div).find('h2').text(section.color);
   })
+}
+
+function lockColor() {
+  const hexCode = $(this).next().text();
+  const color = palette.find(section => section.color === hexCode);
+
+  color.locked = !color.locked;
+  $(this).attr('src', lockImage[color.locked]);
 }
